4c89426f852c4b13a62ea9940b5c594a9f2e550c
M-eporo <gshuji1021@gmail.com>
2025-10-01 21:01:06 +0900

    hourlyWeather

--- src/components/HourlyWeather.tsx
+++ src/components/HourlyWeather.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useAppSelector } from "../store/hooks";
+import { RootState } from "../store/store";
 
 export default function TimelyWeather() {
-
+    const hourlyWeather = useAppSelector((state: RootState) => state.hourlyData.hourlyWeather);
 
     return (
         <View>
             <Text>Timely Weather</Text>
-            
+            <ScrollView horizontal>
+                {hourlyWeather.length <= 0 ?
+                    <Text>No Hour Data Available. Please Enter Your Location.</Text>
+                :
+                    {hourlyWeather.map((hour, index) => (
+                        
+                    ))}
+                }
+            </ScrollView>
         </View>
     )
 }
